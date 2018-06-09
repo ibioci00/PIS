98fa30ab97ea518dffd5438c2ceed5f01c692622
jlonca <jlonca01@fesb.hr>
2018-06-09 18:29:47 +0200

    Initial commit
     Changes to be committed:
    	new file:   PIS-1-master/.gitignore
    	new file:   PIS-1-master/.tfignore
    	new file:   PIS-1-master/PIS-1.sln
    	new file:   PIS-1-master/PIS-1/NewClass.cs
    	new file:   PIS-1-master/PIS-1/PIS-1.csproj
    	new file:   PIS-1-master/PIS-1/Program.cs
    	new file:   PIS-1-master/PIS-1/Properties/AssemblyInfo.cs

---
+++ PIS-1-master/PIS-1/NewClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PIS_1
+{
+    class NewClass
+    {
+        public void proba() { 
+
+        }
+    }
+}
---
+++ PIS-1-master/PIS-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PIS_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("njuberu");
+            //Ovo je zemo VRH
+            //cestitke
+        }
+    }
+}
